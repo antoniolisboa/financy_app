70d3f9061ca5e4fb7f61c772029c8da4459531bc
antoniolisboa <antoniolisboa@alunos.utfpr.edu.br>
2024-02-26 11:43:07 -0300

    organizando o projeto e refatorando

---
+++ lib/common/constants/app_text_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  static const TextStyle bigTextStyle = TextStyle(
+    fontSize: 46,
+    fontWeight: FontWeight.bold,
+  );
+}
